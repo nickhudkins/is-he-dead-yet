dd3ebae170a3a0ceb25ed3ad9958ae9ff25cde4a
Nick Hudkins <nick@nickhudkins.com>
2020-10-10 20:30:12 -0400

    ...

--- index.js
+++ index.js
@@ -2,5 +2,12 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send(`<h1 style="text-align:center;font-size:2em;color:red;">NO</h1>`);
+  res.send(
+    `<h1 style="text-align:center; font-size: 2em; color: red;">NO</h1>`
+  );
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log("Hello world listening on port", port);
 });

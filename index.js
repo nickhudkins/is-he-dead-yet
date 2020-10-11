579c4de64085681d479361609dcfc55a198fbd15
Nick Hudkins <nick@nickhudkins.com>
2020-10-10 20:33:28 -0400

    ...

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get("/", (req, res) => {
   res.send(
-    `<h1 style="text-align:center; font-size: 2em; color: red;">NO</h1>`
+    `<h1 style="text-align:center; font-size: 6em; color: red;">NO</h1>`
   );
 });
 
